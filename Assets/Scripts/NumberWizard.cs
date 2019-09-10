a9aa159b08135db029ef15e52ae7e073a6f51ed8
Pedro Bacchini <pedrobacchini@outlook.com>
2019-09-10 15:07:40 -0300

    Create Project

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI guessText = null;
+
+    [SerializeField] private int max;
+    [SerializeField] private int min;
+    private int _guess;
+
+    private void Start()
+    {
+        NextGuest();
+    }
+
+    public void OnPressHigher()
+    {
+        min = Math.Min(_guess + 1, max);
+        NextGuest();
+    }
+
+    public void OnPressLower()
+    {
+        max = Math.Max(_guess - 1, min);
+        NextGuest();
+    }
+
+    private void NextGuest()
+    {
+//        Include min e exclude max
+        _guess = Random.Range(min, max + 1);
+        guessText.text = _guess.ToString();
+    }
+}
